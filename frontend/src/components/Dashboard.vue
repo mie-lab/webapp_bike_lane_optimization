6c7296f82b0a0ce75bf0ff24763c7f07fd70c43f
Mischa <mbauckhage@student.ethz.ch>
2024-03-08 11:37:14 +0100

    dashboard button

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,65 +1,66 @@
 <template>
-    <div class="dashboard-container">
-        <div class="dashboard-navigation">
-            <button class="close-open" @click ="toggleDashboard">close</button>
-        </div>
-        <div v-show ="dashboardStatus" class="dashboard-content">
-            <h1>Dashboard</h1>
-            <p>Dashboard content goes here</p>
-        </div>
+  <div class="dashboard-container">
+    <div class="dashboard-navigation" @click="toggleDashboard">
+      <div class="angle-div">
+        <i class="fa-solid fa-angle-left"></i>
+      </div>
     </div>
+    <div v-show="dashboardStatus" class="dashboard-content">
+      <h1>Dashboard</h1>
+      <p>Dashboard content goes here</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Dashboard',
-    data() {
-        return {
-            dashboardStatus: true,
-        };
+  name: "Dashboard",
+  data() {
+    return {
+      dashboardStatus: true,
+    };
+  },
+  methods: {
+    toggleDashboard() {
+      this.dashboardStatus = !this.dashboardStatus;
     },
-    methods: {
-        toggleDashboard() {
-            this.dashboardStatus = !this.dashboardStatus;
-        }
-        // Your methods go here
-    },
-    mounted() {
-        // Code to run when the component is mounted goes here
-    },
+    // Your methods go here
+  },
+  mounted() {
+    // Code to run when the component is mounted goes here
+  },
 };
 </script>
 
 <style scoped>
 .dashboard-container {
-    display: flex; /* Enables Flexbox layout */
-    height: 100vh; /* Full viewport height */
-    position: absolute;
-    right:0;
-    
-  
-
+  display: flex; /* Enables Flexbox layout */
+  height: 100vh; /* Full viewport height */
+  position: absolute;
+  right: 0;
 }
 
 .dashboard-navigation {
-    background-color: #959595;
-    height: 100%; /* Take full height of the parent */
-    width: 20px; /* Adjusted for visibility */
-    display: flex; /* Enables Flexbox layout */
-    justify-content: center; /* Centers items horizontally */
-    align-items: center; /* Centers items vertically */
-    flex-direction: column; /* Stacks items vertically */
-    
+  background-color: rgba(149, 149, 149, 0.5);
+  height: 100%; /* Take full height of the parent */
+  width: 50px; /* Adjusted for visibility */
+  display: flex; /* Enables Flexbox layout */
+  justify-content: center; /* Centers items horizontally */
+  align-items: center; /* Centers items vertically */
+  flex-direction: column; /* Stacks items vertically */
 }
 
 .dashboard-content {
-    background-color: #cacaca;
-    flex-grow: 1; 
-    width: 300px;
+  background-color: #cacaca;
+  flex-grow: 1;
+  width: 300px;
 }
 .close-open {
-    background-color: #e16ece;
-    
+  background-color: #e16ece;
 }
 
+.angle-div {
+  z-index: 10;
+  background-color: red;
+}
 </style>
