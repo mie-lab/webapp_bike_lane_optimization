9237af67f619994f5d011492a92ea3b080ef8f5e
Elina <selina@student.ethz.ch>
2024-03-18 16:37:20 +0100

    User Input split in two pages

--- frontend/src/components/UserInput.vue
+++ frontend/src/components/UserInput.vue
@@ -4,34 +4,18 @@
       <i class="fa-solid fa-times" style="font-size: 24px"></i>
     </button>
     <h1 class="text-pink">User Input</h1>
-    <div class="user-input-container">
-      <h2 class="text-blue">Car travel time weighting</h2>
-      <div class="slide-container">
-        <input
-          class="slider"
-          type="range"
-          min="0"
-          max="1"
-          step="0.01"
-          v-model="timeWeighting"
-          @click="setTimeWeight(timeWeighting)"
-        />
-      </div>
-      <p>Chosen weighting: {{ timeWeighting }}</p>
-      <br />
-      <h2 class="text-blue">Allocation of lanes</h2>
-      <div class="slide-container">
-        <input
-          class="slider"
-          type="range"
-          min="0"
-          max="100"
-          v-model="laneAllocation"
-          @click="setLaneAllocation(laneAllocation)"
-        />
-      </div>
-      <p>Chosen Value in percent: {{ laneAllocation }} %</p>
-      <br />
+    <div v-show="!statusStore.userInputSide" class="user-input-container">
+     
+      <p> Create your own project and run the optimization algorithm for an area of your choice. </p>
+
+      <h2 class="text-blue">Project name</h2>
+      <input
+        class="project-name-input"
+        type="text"
+        v-model="projectName"
+        @input="setProjectName($event.target.value)"
+      />
+
       <h2 class="text-blue">Area of interest</h2>
       <div class="button-container">
         <button
@@ -60,29 +44,25 @@
         </button>
       </div>
       <br />
-      <h2 class="text-blue">Project name</h2>
-      <input
-        class="project-name-input"
-        type="text"
-        v-model="projectName"
-        @input="setProjectName($event.target.value)"
-      />
+      
 
       <br />
       <button
         :class="{ 'disabled-button': isButtonDisabled || !projectName }"
-        @click="runConstructGraph"
+        @click="toggleUserInputNextSide"
       >
-        Run
+        Continue
       </button>
 
-      <ring-loader :loading="loading" :color="color" :size="size"></ring-loader>
     </div>
+    <div v-show="statusStore.userInputSide"> 
+      <UserInputSecondPageVue />
+    </div>
+
   </div>
 </template>
 
 <script>
-import { runConstructGraph, runOptimization } from "../scripts/api.js";
 import { userInputStore } from "../stores/userInputStore.js";
 import {
   enableDrawRectangle,
@@ -93,12 +73,13 @@ import {
 import { statusVariablesStore } from "../stores/statusVariablesStore.js";
 import { mapStore } from "../stores/mapStore.js";
 import { useResultsStore } from "../stores/algorithmResultsStore.js";
-import PulseLoader from "vue-spinner/src/PulseLoader.vue";
-import RingLoader from "vue-spinner/src/RingLoader.vue";
-import BounceLoader from "vue-spinner/src/BounceLoader.vue";
+import UserInputSecondPageVue from './UserInputSecondPage.vue';
 
 export default {
   name: "UserInput",
+  components: {
+    UserInputSecondPageVue,
+  },
   setup() {
     const statusStore = statusVariablesStore();
     const inputStore = userInputStore();
@@ -109,23 +90,28 @@ export default {
       resultsStore,
       projectName: inputStore.projectName,
       setProjectName: inputStore.setProjectName,
+      continue: statusStore.userInputSide,
     };
   },
   data() {
     const inputStore = userInputStore();
+    const statusStore = statusVariablesStore(); 
     return {
-      timeWeighting: 0.7,
-      laneAllocation: 10,
       isButtonDisabled: true,
       projectName: inputStore.projectName,
       color: "#da5268",
       size: "25px",
+      continue: statusStore.userInputSide,
+      
     };
   },
-  components: {
-    RingLoader,
-  },
+
   methods: {
+    
+    toggleUserInputNextSide(){
+      const statusStore = statusVariablesStore();
+      statusStore.changeUserInputSide();
+    },
     toggleTabsVisibility() {
       const statusStore = statusVariablesStore();
       statusStore.toggleTabsVisibility();
@@ -138,82 +124,8 @@ export default {
       drawPolygon();
       this.isButtonDisabled = false;
     },
-    setTimeWeight(value) {
-      const inputStore = userInputStore();
-      inputStore.setTimeWeighting(value);
-    },
-    setLaneAllocation(value) {
-      const inputStore = userInputStore();
-      inputStore.setLaneAllocation(value);
-    },
+   
 
-    async runConstructGraph() {
-      const inputStore = userInputStore();
-      const resultsStore = useResultsStore();
-
-      try {
-        const response = await runConstructGraph(
-          inputStore.boundingBox,
-          inputStore.projectName
-        );
-        const result = {
-          expected_runtime: response.expected_runtime,
-          project_name: inputStore.projectName,
-          variables: response.variables,
-          internal_project_name: JSON.stringify(inputStore.boundingBox),
-          runs: [],
-        };
-        resultsStore.addResult(result);
-        console.log("Response:", result);
-
-        // Display a confirmation dialog
-        const continueOperation = window.confirm(
-          `Running the algorithm will take ${result.expected_runtime.toFixed(2)} seconds. Do you want to continue?`
-        );
-
-        // Check user's response
-        if (continueOperation) {
-          // User clicked OK, continue with the operation
-          console.log("Operation continued!");
-          this.callOptimization();
-        } else {
-          // User clicked Cancel or closed the dialog, abort the operation
-          console.log("Operation aborted!");
-        }
-      } catch (error) {
-        console.error("Error:", error.message);
-      }
-    },
-
-    async callOptimization() {
-      const inputStore = userInputStore();
-      const resultsStore = useResultsStore();
-      const project_name = inputStore.projectName;
-      const algorithm = "betweenness_biketime";
-      const bikeRatio = inputStore.laneAllocation;
-      const carWeight = inputStore.timeWeighting;
-      const bikeSafetyPenatly = 2;
-      const optimizeFrequency = 30;
-      try {
-        const response = await runOptimization(
-          project_name,
-          algorithm,
-          bikeRatio,
-          optimizeFrequency,
-          carWeight,
-          bikeSafetyPenatly
-        );
-
-        const result = {
-          bikeEdges: response.bike_edges,
-          variables: response.run_name,
-        };
-        resultsStore.addRunResult(response.project_name, result);
-        console.log("Response:", result);
-      } catch (error) {
-        console.error("Error:", error.message);
-      }
-    },
   },
 };
 </script>
@@ -223,8 +135,8 @@ export default {
 @import "../styles/SideBarStyle.css";
 
 .disabled-button {
-  background-color: #ccc; /* Grey color */
-  color: #666; /* Darker grey color for text */
-  cursor: not-allowed; /* Change cursor to not-allowed */
+  background-color: #ccc; 
+  color: #666; 
+  cursor: not-allowed; 
 }
 </style>
---
+++ frontend/src/components/UserInputSecondPage.vue
@@ -0,0 +1,205 @@
+<template>
+  <div>
+    <button class="close-btn" @click="toggleTabsVisibility">
+      <i class="fa-solid fa-times" style="font-size: 24px"></i>
+    </button>
+    
+    <div class="user-input-container">
+      <p> You can manually change the values for car travel time weighting or lane allocation. 
+        <br /><br />
+       Click <a href="#" @click="toggleActiveTab('Help')"> here</a> for more information on the impact of these parameters.
+       
+        
+         </p>
+      <h2 class="text-blue">Car travel time weighting</h2>
+      <div class="slide-container">
+        <input
+          class="slider"
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          v-model="timeWeighting"
+          @click="setTimeWeight(timeWeighting)"
+        />
+      </div>
+      <p>Chosen weighting: {{ timeWeighting }}</p>
+      <br />
+      <h2 class="text-blue">Allocation of lanes</h2>
+      <div class="slide-container">
+        <input
+          class="slider"
+          type="range"
+          min="0"
+          max="100"
+          v-model="laneAllocation"
+          @click="setLaneAllocation(laneAllocation)"
+        />
+      </div>
+      <p>Chosen Value in percent: {{ laneAllocation }} %</p>
+      <br />
+      
+      <br />
+
+      <br />
+      <button @click="toggleUserInputNextSide" class="back-button">Back</button>
+
+      <button
+        
+        @click="runConstructGraph"
+      >
+        Run
+      </button>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { runConstructGraph, runOptimization } from "../scripts/api.js";
+import { userInputStore } from "../stores/userInputStore.js";
+import { statusVariablesStore } from "../stores/statusVariablesStore.js";
+import { useResultsStore } from "../stores/algorithmResultsStore.js";
+
+import RingLoader from "vue-spinner/src/RingLoader.vue";
+
+
+export default {
+  name: "UserInputSecondPage",
+  setup() {
+    const statusStore = statusVariablesStore();
+    const inputStore = userInputStore();
+    const resultsStore = useResultsStore();
+
+    return {
+      statusStore,
+      resultsStore,
+      projectName: inputStore.projectName,
+      setProjectName: inputStore.setProjectName,
+    };
+  },
+  data() {
+    const inputStore = userInputStore();
+    return {
+      timeWeighting: 0.7,
+      laneAllocation: 10,
+      isButtonDisabled: true,
+      projectName: inputStore.projectName,
+      color: "#da5268",
+      size: "25px",
+
+    };
+  },
+  components: {
+    RingLoader,
+  },
+  methods: {
+    toggleActiveTab(tab) {
+      // Toggle the active tab using the Pinia store
+      const statusStore = statusVariablesStore();
+      const storedTab = this.activeTab;
+      if (tab == storedTab) {
+        statusStore.setActiveTab("None");
+      } else {
+        statusStore.setActiveTab(tab);
+      }
+    },
+    toggleUserInputNextSide(){
+      const statusStore = statusVariablesStore();
+      statusStore.changeUserInputSide();
+    },
+    
+    toggleTabsVisibility() {
+      const statusStore = statusVariablesStore();
+      statusStore.toggleTabsVisibility();
+    },
+    
+    setTimeWeight(value) {
+      const inputStore = userInputStore();
+      inputStore.setTimeWeighting(value);
+    },
+    setLaneAllocation(value) {
+      const inputStore = userInputStore();
+      inputStore.setLaneAllocation(value);
+    },
+
+    async runConstructGraph() {
+      const inputStore = userInputStore();
+      const resultsStore = useResultsStore();
+
+      try {
+        const response = await runConstructGraph(
+          inputStore.boundingBox,
+          inputStore.projectName
+        );
+        const result = {
+          expected_runtime: response.expected_runtime,
+          project_name: inputStore.projectName,
+          variables: response.variables,
+          internal_project_name: JSON.stringify(inputStore.boundingBox),
+          runs: [],
+        };
+        resultsStore.addResult(result);
+        console.log("Response:", result);
+
+        // Display a confirmation dialog
+        const continueOperation = window.confirm(
+          `Running the algorithm will take ${result.expected_runtime.toFixed(2)} seconds. Do you want to continue?`
+        );
+
+        // Check user's response
+        if (continueOperation) {
+          // User clicked OK, continue with the operation
+          console.log("Operation continued!");
+          this.callOptimization();
+        } else {
+          // User clicked Cancel or closed the dialog, abort the operation
+          console.log("Operation aborted!");
+        }
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    },
+
+    async callOptimization() {
+      const inputStore = userInputStore();
+      const resultsStore = useResultsStore();
+      const project_name = inputStore.projectName;
+      const algorithm = "betweenness_biketime";
+      const bikeRatio = inputStore.laneAllocation;
+      const carWeight = inputStore.timeWeighting;
+      const bikeSafetyPenatly = 2;
+      const optimizeFrequency = 30;
+      try {
+        const response = await runOptimization(
+          project_name,
+          algorithm,
+          bikeRatio,
+          optimizeFrequency,
+          carWeight,
+          bikeSafetyPenatly
+        );
+
+        const result = {
+          bikeEdges: response.bike_edges,
+          variables: response.run_name,
+        };
+        resultsStore.addRunResult(response.project_name, result);
+        console.log("Response:", result);
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "../styles/UserInputStyles.css";
+@import "../styles/SideBarStyle.css";
+
+.back-button {
+  background-color: #ccc; 
+  color: #666; 
+}
+</style>
