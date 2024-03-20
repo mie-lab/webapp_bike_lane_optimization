c7c860ad09e7b0f242421a20dd9ddcd07f2d0793
Mischa <mbauckhage@student.ethz.ch>
2024-03-20 13:21:28 +0100

    map tests

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -4,22 +4,28 @@
 
 <script>
 import "ol/ol.css";
-import proj4 from 'proj4';
+import proj4 from "proj4";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
 import { enableDrawRectangle, onDrawCreate } from "../scripts/draw.js";
 import { userInputStore } from "../stores/userInputStore.js";
 import { statusVariablesStore } from "../stores/statusVariablesStore.js";
 import { mapStore } from "../stores/mapStore.js";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import TileWMS from "ol/source/TileWMS";
+import WMS from "ol/source/ImageWMS";
 
 // Define projection definitions
 proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs"); // WGS84
-proj4.defs("EPSG:2056", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
-
+proj4.defs(
+  "EPSG:2056",
+  "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs"
+);
 
 // Function to convert coordinates from EPSG:4326 to EPSG:2056
 function convertCoordinatesTo2056(coordinates) {
-    return coordinates.map(coord => proj4("EPSG:4326", "EPSG:2056", coord));
+  return coordinates.map((coord) => proj4("EPSG:4326", "EPSG:2056", coord));
 }
 
 export default {
@@ -54,6 +60,22 @@ export default {
     const mapStoreInstance = mapStore();
     mapStoreInstance.setMap(this.map);
 
+    this.map.on("load", () => {
+      this.map.addSource("wms-layer", {
+        type: "raster",
+        tiles: [
+          "https://baug-ikg-gis-01.ethz.ch:8443/geoserver/GMP_EBC/ows?SERVICE=WMS&?layers=GMP_EBC:v_optimized&bbox=2485014.052451379,1074188.6943776933,2837016.9329778464,1299782.763494124&width=768&height=492&srs=EPSG:2056&styles=&format=image/png",
+        ],
+        tileSize: 256,
+      });
+      this.map.addLayer({
+        id: "wms-layer",
+        type: "raster",
+        source: "wms-layer",
+        paint: {},
+      });
+    });
+
     this.map.on("draw.create", (event) => {
       onDrawCreate(event, this);
       if (statusStore.drawingRectangleEnabled) {
@@ -77,7 +99,9 @@ export default {
 
     saveBoundingBox(boundingBox) {
       const coordinatesWithoutLast = boundingBox.slice(0, -1); // don't save the last point
-      const convertedCoordinates = convertCoordinatesTo2056(coordinatesWithoutLast); // transform coordinates to EPSG:2056
+      const convertedCoordinates = convertCoordinatesTo2056(
+        coordinatesWithoutLast
+      ); // transform coordinates to EPSG:2056
 
       const inputStore = userInputStore();
       inputStore.setBoundingBox(convertedCoordinates);
