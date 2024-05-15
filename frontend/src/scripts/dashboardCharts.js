4c46a1d02439fcaf54a3a7f1e0bc5289fd573fa7
eelliinnaa <e.scheiring@gmail.com>
2024-05-15 14:57:58 +0200

    colors update

--- frontend/src/scripts/dashboardCharts.js
+++ frontend/src/scripts/dashboardCharts.js
@@ -3,80 +3,87 @@ import Chart from "chart.js/auto";
 export function createPieChartComplexity(resultStore,compareRunStore,isOpenComplexity,canvasBike,canvasCar,canvasCompareBike,canvasCompareCar){
   let purpleShades = [
     '#da5268', // Original color
+    '#daa068',
     '#da5868', 
+    '#da9a68', 
     '#da5e68', 
-    '#da6468', 
+    '#da9468', 
+    '#da6468',
+    '#da8e68', 
     '#da6a68', 
     '#da7068', 
-    '#da7668', 
-    '#da7c68', 
-    '#da8268', 
+    '#da7668',
     '#da8868', 
-    '#da8e68', 
-    '#da9468', 
-    '#da9a68', 
-    '#daa068', 
+    '#da8268', 
+    '#da7c68',
+     
+    
+    
+     
   ];
   let blueShades = [
     '#0d00a3', // Original color
-    '#0d06a3', 
+    '#0d4ea3', 
+    '#0d06a3',
+    '#0d48a3', 
+    '#0d2aa3',
     '#0d0ca3', 
+    '#0d42a3', 
     '#0d12a3', 
+    '#0d3ca3',
     '#0d18a3', 
     '#0d1ea3', 
-    '#0d24a3', 
-    '#0d2aa3', 
+    '#0d24a3',
+    '#0d36a3',  
     '#0d30a3', 
-    '#0d36a3', 
-    '#0d3ca3', 
-    '#0d42a3', 
-    '#0d48a3', 
-    '#0d4ea3', 
+      
+    
   ];
     let lightGreenShades = [
       '#a7d479', // Original color
-      '#aed479', 
-      '#b5d479', 
-      '#bcd479', 
-      '#c3d479', 
-      '#cad479', 
-      '#d1d479', 
-      '#d8d479', 
-      '#d4d47f', 
-      '#d4d486', 
-      '#d4d48d', 
-      '#d4d494', 
-      '#d4d49b', 
       '#d4d4a2', 
+      '#aed479', 
+      '#d4d49b', 
+      '#b5d479',
+      '#d4d494', 
+      '#bcd479', 
+      '#d4d48d', 
+      '#c3d479',
+      '#d4d486',  
+      '#cad479', 
+      '#d1d479',
+      '#d4d47f', 
+      '#d8d479',  
     ];
     let darkGreenShades = [
       '#3d7748', // Original color
+      '#004824',
       '#367248', 
-      '#2f6d48', 
-      '#286848', 
-      '#216348', 
-      '#1a5e48', 
-      '#135948', 
-      '#0c5448', 
-      '#054f48', 
-      '#004a48', 
-      '#00483f', 
-      '#004836', 
       '#00482d', 
-      '#004824', 
+      '#2f6d48', 
+      '#004836', 
+      '#286848',
+      '#00483f', 
+      '#216348', 
+      '#004a48', 
+      '#1a5e48', 
+      '#054f48',
+      '#135948', 
+      '#0c5448',   
+      
     ];
 
         if (isOpenComplexity) {
           
             // Bike pie chart
-            createSinglePieChart(Object.keys(resultStore.complexity.bike),
+            createSinglePieChart(Object.keys(resultStore.complexity.bike).map(value => value +" degree intersections"),
                 Object.values(resultStore.complexity.bike).map(value => value * 100),
                 canvasBike,
                 "Bike Complexity",
                 purpleShades);
             
             // Car pie chart
-               createSinglePieChart(Object.keys(resultStore.complexity.car),
+               createSinglePieChart(Object.keys(resultStore.complexity.car).map(value => value +" degree intersections"),
                 Object.values(resultStore.complexity.car).map(value => value * 100),
                 canvasCar,
                 "Car Complexity",
@@ -85,14 +92,14 @@ export function createPieChartComplexity(resultStore,compareRunStore,isOpenCompl
 
             if (compareRunStore.compare) {
                 // Bike pie chart
-                createSinglePieChart(Object.keys(compareRunStore.complexity.bike),
+                createSinglePieChart(Object.keys(compareRunStore.complexity.bike).map(value => value +" degree intersections"),
                     Object.values(compareRunStore.complexity.bike).map(value => value * 100),
                     canvasCompareBike,
                     "Bike Complexity",
                     lightGreenShades);
                 
                 // Car pie chart
-                createSinglePieChart(Object.keys(compareRunStore.complexity.car),
+                createSinglePieChart(Object.keys(compareRunStore.complexity.car).map(value => value +" degree intersections"),
                     Object.values(compareRunStore.complexity.car).map(value => value * 100),
                     canvasCompareCar,
                     "Car Complexity",
@@ -119,7 +126,7 @@ function createSinglePieChart(labels,dataset,canvas,chartTitle,colorShades){
                 {
                 data: dataset,
                 backgroundColor: colorShades,
-                label: "Percentage with given complexity",
+                label: "[%] ",
                 },
             ],
             },
@@ -242,7 +249,7 @@ export function createDoughnutChart(labels, dataset, colors, canvas, chartTitle)
             labels: labels,
             datasets: [
                 {
-                  label: "Distances in km",
+                  label: "[km]",
                   backgroundColor: colors,
                   data: dataset,
                 },
