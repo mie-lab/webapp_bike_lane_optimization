c0315cae016df195c2f2360a0610fd013995931e
Leonard Haas <lehaas@ethz.ch>
2025-05-26 16:21:27 +0200

    typo

--- frontend/src/strings/projectInfoText.js
+++ frontend/src/strings/projectInfoText.js
@@ -1,5 +1,5 @@
 export const projectInfoText = {
   text1: "Welcome to the Zurich Bike Lane Optimizer!",
-  text2: "This web application was orinally developed as a master project by Mischa Bauckhage and Elina Scheiring at the Chair of Geoinformation Engineering, ETH Zurich. It was further extended and refined by Leonard Haas in a follow-up master project. Supervised by Nina Wiedemann and Ayda Grisiute.",
+  text2: "This web application was originally developed as a master project by Mischa Bauckhage and Elina Scheiring at the Chair of Geoinformation Engineering, ETH Zurich. It was further extended and refined by Leonard Haas in a follow-up master project. Supervised by Nina Wiedemann and Ayda Grisiute.",
   contactInfo: "For any questions or additional information contact mbauckhage@ethz.ch, selina@ethz.ch or lehaas@ethz.ch.",
 };
